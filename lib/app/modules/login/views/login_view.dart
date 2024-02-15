ec941adaaf43d2563c6d9fa69aa935994b0a01d9
jamjam88 <zamcool15@gmail.com>
2024-02-15 13:46:03 +0700

    update

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -11,33 +11,62 @@ class LoginView extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         title: const Text('LoginView'),
         centerTitle: true,
       ),
+      backgroundColor: Colors.black54,
       body: Center(
         child: Form(key: controller.formkey,child: Column(
           children: [
             TextFormField(
-              controller: controller.usernameController,
-              decoration: InputDecoration(hintText: "Masukkan Username"),
-              validator: (value){
-                if(value!.length<2){
-                  return "Username tidak boleh kosong";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: controller.passwordController,
-              decoration: InputDecoration(hintText: "Masukkan Password"),
-              validator: (value){
-                if(value!.length<2){
-                  return "Password tidak boleh kosong";
-                }
-                return null;
-              },
-            ),
-            Obx(() => controller.loading.value
+                controller: controller.usernameController,
+                decoration: InputDecoration(
+                    hintText: "Masukkan Username",
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintStyle: const TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(),
+                    ),
+                    labelText: 'Masukkan Username'),
+                validator: (value) {
+                  if (value!.length < 2) {
+                    return "Username tidak boleh kosong";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15,),
+              TextFormField(
+                controller: controller.passwordController,
+                decoration: InputDecoration(
+                    hintText: "Masukkan Password",
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintStyle: const TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(),
+                    ),
+                    labelText: 'Masukkan Password'),
+                validator: (value) {
+                  if (value!.length < 2) {
+                    return "Password tidak boleh kosong";
+                  }
+                  return null;
+                },
+              ),
+              Obx(() => controller.loading.value
                 ? CircularProgressIndicator()
                 : ElevatedButton(
                 onPressed: () {
