f036200229da7f60e466740d7b85d3ccbd5dd7ec
jamjam88 <zamcool15@gmail.com>
2024-02-19 14:29:57 +0700

    update

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -7,6 +7,7 @@ import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
   const LoginView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,66 +18,92 @@ class LoginView extends GetView<LoginController> {
       ),
       backgroundColor: Colors.black54,
       body: Center(
-        child: Form(key: controller.formkey,child: Column(
-          children: [
-            TextFormField(
-                controller: controller.usernameController,
-                decoration: InputDecoration(
-                    hintText: "Masukkan Username",
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintStyle: const TextStyle(color: Colors.white),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(),
-                    ),
-                    labelText: 'Masukkan Username'),
-                validator: (value) {
-                  if (value!.length < 2) {
-                    return "Username tidak boleh kosong";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 15,),
-              TextFormField(
-                controller: controller.passwordController,
-                decoration: InputDecoration(
-                    hintText: "Masukkan Password",
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintStyle: const TextStyle(color: Colors.white),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(),
-                    ),
-                    labelText: 'Masukkan Password'),
-                validator: (value) {
-                  if (value!.length < 2) {
-                    return "Password tidak boleh kosong";
-                  }
-                  return null;
-                },
-              ),
-              Obx(() => controller.loading.value
-                ? CircularProgressIndicator()
-                : ElevatedButton(
-                onPressed: () {
-                  controller.login();
-                },
-                child: Text("Login")),),
-            ElevatedButton(onPressed: ()=>Get.toNamed(Routes.REGISTER),
-                child: Text("Register"))
-          ],
-        ),),),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Form(
+            key: controller.formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 400,
+                  width: 400,
+                    child: Image.asset(
+                  'assets/Logo Tok Putih.png'
+                )),
+                SizedBox(
+                  height: 80,
+                ),
+                TextFormField(
+                  controller: controller.usernameController,
+                  decoration: InputDecoration(
+                      hintText: "Masukkan Username",
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintStyle: const TextStyle(color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(),
+                      ),
+                      labelText: 'Masukkan Username'),
+                  validator: (value) {
+                    if (value!.length < 2) {
+                      return "Username tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  controller: controller.passwordController,
+                  decoration: InputDecoration(
+                      hintText: "Masukkan Password",
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintStyle: const TextStyle(color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(),
+                      ),
+                      labelText: 'Masukkan Password'),
+                  validator: (value) {
+                    if (value!.length < 2) {
+                      return "Password tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Obx(
+                  () => controller.loading.value
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                          onPressed: () {
+                            controller.login();
+                          },
+                          child: Text("Login")),
+                ),
+                SizedBox(height: 8,),
+                ElevatedButton(
+                    onPressed: () => Get.toNamed(Routes.REGISTER),
+                    child: Text("Register"))
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
